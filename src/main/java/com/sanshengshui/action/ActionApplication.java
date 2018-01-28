266b3349b0ef87025244214f0f46aa2249446a93
穆书伟 <james@igelian.com>
2018-01-28 22:48:39 +0800

    更改项目框架

---
+++ src/main/java/com/sanshengshui/action/ActionApplication.java
@@ -0,0 +1,12 @@
+package com.sanshengshui.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionApplication.class, args);
+	}
+}
