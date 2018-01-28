8bc6fa6fa46e2181ac2b4d330f86ff5aed3b1aa2
sanshengshui <lovewsic@gmail.com>
2018-01-28 23:21:19 +0800

    banner启动图形和基础类型

--- src/main/java/com/sanshengshui/action/ActionApplication.java
+++ src/main/java/com/sanshengshui/action/ActionApplication.java
@@ -1,11 +1,23 @@
 package com.sanshengshui.action;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ActionApplication {
 
+	@Value("${book.author}")
+	private String bookAuthor;
+	@Value("${book.name}")
+	private String bookName;
+	@RequestMapping("/")
+	String index(){
+		return "book name is:"+bookName+" and book author is:" + bookAuthor;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ActionApplication.class, args);
 	}
