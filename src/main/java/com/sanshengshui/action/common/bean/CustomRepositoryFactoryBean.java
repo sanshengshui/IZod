ef23adc9a82c74c9bdf3436bcf60471489f07b85
sanshengshui <lovewsic@gmail.com>
2018-02-05 11:46:53 +0800

    添加Spring Data Jpa的redis支持

--- src/main/java/com/sanshengshui/action/common/bean/CustomRepositoryFactoryBean.java
+++ src/main/java/com/sanshengshui/action/common/bean/CustomRepositoryFactoryBean.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.repository.core.RepositoryInformation;
 import org.springframework.data.repository.core.RepositoryMetadata;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
--- src/main/java/com/sanshengshui/action/controller/CacheDemoController.java
+++ src/main/java/com/sanshengshui/action/controller/CacheDemoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CacheDemoController {
+
     @Autowired
     CacheDemoService cacheDemoService;
 
