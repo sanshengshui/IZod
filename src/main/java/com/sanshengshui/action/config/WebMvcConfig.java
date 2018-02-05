f2d8f98c00b01450f0d28b0a4749085896af44b0
sanshengshui <lovewsic@gmail.com>
2018-02-05 19:08:07 +0800

    添加js

--- src/main/java/com/sanshengshui/action/config/WebMvcConfig.java
+++ src/main/java/com/sanshengshui/action/config/WebMvcConfig.java
@@ -9,5 +9,6 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/ws").setViewName("/ws");
+        registry.addViewController("/data").setViewName("/data");
     }
 }
